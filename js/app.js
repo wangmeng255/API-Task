84ecec837017a1d0ad5a36c9bb8a39ee42f568b8
JP DeVries <mail@devries.jp>
2016-06-08 14:54:55 -0700

    Fixing Video

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 $(function() {
 	$(".iframe").fancybox();
+	$("#tip4").fancybox({
+			'padding'		: 0,
+			'autoScale'		: false,
+			'transitionIn'	: 'none',
+			'transitionOut'	: 'none',
+			'title'			: this.title,
+			'width'		: 854,
+			'height'		: 480,
+			'content':'<iframe width="854" height="480" src="https://www.youtube.com/embed/' + $("#tip4").attr('data-video-id') + '" frameborder="0" allowfullscreen></iframe>',
+			//'href'			: this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+			'type'			: 'iframe',
+	});
+
 	$("#search-item").submit(function(event) {
 		event.preventDefault();
 		var searchItem = $("#query").val();
@@ -16,19 +29,21 @@ $(function() {
 		$.getJSON(url, params, function(data) {
 			console.log(data.items);
 			showResults(data.items);
+
 		});
 	}
 	function showResults(items)
 	{
-		$("#search-results").append('<a class="iframe" href="http://www.youtube.com/embed/L9szn1QQfas?autoplay=1">Youtube (iframe)</a>');
+		//$("#search-results").append('<a class="iframe" href="http://www.youtube.com/embed/L9szn1QQfas?autoplay=1">Youtube (iframe)</a>');
 		/*
 		$.each(items, function(i, val) {
-			$("#search-results").append("<a class='iframe' href='//youtube.com/embed/" + 
+			$("#search-results").append("<a class='iframe' href='//youtube.com/embed/" +
 				val.id.videoId + "'><img src='" +
 				val.snippet.thumbnails.default.url + "' class='thumbnails' width='"+
 				val.snippet.thumbnails.default.width +"' height='" +
 				val.snippet.thumbnails.default.height + "' title='" +
 				val.snippet.title + "'></a>");
 		});*/
+		$(".iframe").fancybox();
 	}
 });
